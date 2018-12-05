24cf88e92f839eff3accdb828eb87b5bc4aaf702
shashibabu <shashibabu66@gmail.com>
2018-12-05 07:15:23 +0530

    my first commit

---
+++ pythontest.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+print("this is my first test")
+name="how are you"
+print ("hello")
+print ("shashank " +name)
+print ("hello \n madhu")
+print ("shashi \t babu")
+print ("welcome to \"python\" class")
+print ("madhu")
